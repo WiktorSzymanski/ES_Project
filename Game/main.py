7ee1faa376960afe6c6b01df1e4306514fb042e1
DarkVikos <darkvikos@gmail.com>
2022-12-03 08:53:06 +0100

    player controlled 'snake' is in another file. deleted not necesery code

--- Game/main.py
+++ Game/main.py
@@ -3,177 +3,7 @@ import pygame
 from sys import exit
 from random import randint
 
-class Snake(pygame.sprite.Sprite):
-  def __init__(self):
-    super().__init__()
-    self.headPosition = pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2)
-    self.tailTab = [pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2 + 1),pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2 + 2)]
-
-    self.endOfTailLastPosition = pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2)
-    self.direction = 'up'
-    self.newDirection = 'up'
-    self.snakeScale = (TILE_WIDTH / 40)
-
-    self.snakeHeadImg = pygame.image.load('./graphics/head.png').convert_alpha()
-    self.snakeHeadImg = pygame.transform.rotozoom(self.snakeHeadImg, 0, self.snakeScale)
-    self.snakeHeadImgTab = [self.snakeHeadImg, pygame.transform.rotate(self.snakeHeadImg, 270), pygame.transform.rotate(self.snakeHeadImg, 180), pygame.transform.rotate(self.snakeHeadImg, 90)]
-
-    self.snakeBodyImg = pygame.image.load('./graphics/body.png').convert_alpha()
-    self.snakeBodyImg = pygame.transform.rotozoom(self.snakeBodyImg, 0, self.snakeScale)
-    self.snakeBodyImgTab = [self.snakeBodyImg, pygame.transform.rotate(self.snakeBodyImg, 270), pygame.transform.rotate(self.snakeBodyImg, 180), pygame.transform.rotate(self.snakeBodyImg, 90)]
-
-    self.snakeTurnLeftImg = pygame.image.load('./graphics/turn.png').convert_alpha()
-    self.snakeTurnLeftImg = pygame.transform.rotozoom(self.snakeTurnLeftImg, 0, self.snakeScale)
-    self.snakeTurnLeftImgTab = [self.snakeTurnLeftImg, pygame.transform.rotate(self.snakeTurnLeftImg, 270), pygame.transform.rotate(self.snakeTurnLeftImg, 180), pygame.transform.rotate(self.snakeTurnLeftImg, 90)]
-
-    self.snakeTurnRightImgTab = [pygame.transform.flip(self.snakeTurnLeftImgTab[0], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[1], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[2], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[3], False, True)]
-
-    self.snakeTailImg = pygame.image.load('./graphics/tail.png').convert_alpha()
-    self.snakeTailImg = pygame.transform.rotozoom(self.snakeTailImg, 0, self.snakeScale)
-    self.snakeTailImgTab = [self.snakeTailImg, pygame.transform.rotate(self.snakeTailImg, 270), pygame.transform.rotate(self.snakeTailImg, 180), pygame.transform.rotate(self.snakeTailImg, 90)]
-
-  def update(self):
-    self.drawSnake()
-    self.playerInput()    
-
-  def drawSnake(self):
-    snakeHeadRect = self.snakeHeadImg.get_rect(center = (self.headPosition[0] * TILE_WIDTH + 50 + TILE_WIDTH / 2, self.headPosition[1] * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
-    WINDOW.blit(self.snakeHeadImg, snakeHeadRect)
-
-    for tile in self.tailTab:
-      #pygame.draw.rect(WINDOW, (0, 0, 255), pygame.Rect((tile[0] * TILE_WIDTH) + 50, (tile[1] * TILE_HEIGHT) + 50, TILE_WIDTH, TILE_HEIGHT ))
-      self.imgVariant()
-
-  def drawTail(self, ckBefore, position):
-    if (ckBefore == (1, 0)):
-      img = self.snakeTailImgTab[1]
-    elif (ckBefore == (-1, 0)):
-      img = self.snakeTailImgTab[3]
-    elif (ckBefore == (0, 1)):
-      img = self.snakeTailImgTab[2]
-    elif (ckBefore == (0, -1)):
-      img = self.snakeTailImgTab[0]
-
-    snakeBodyRect = img.get_rect(center = (position.x * TILE_WIDTH + 50 + TILE_WIDTH / 2, position.y * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
-    WINDOW.blit(img, snakeBodyRect)
-
-  def imgVariantDraw(self, ckBefore, ckAfter, position):
-    if (ckBefore == ckAfter == (-1, 0)):
-      img = self.snakeBodyImgTab[1]
-    elif (ckBefore == ckAfter == (1, 0)):
-      img = self.snakeBodyImgTab[3]
-    elif (ckBefore == ckAfter == (0, -1)):
-      img = self.snakeBodyImgTab[2]
-    elif (ckBefore == ckAfter == (0, 1)):
-      img = self.snakeBodyImgTab[0]
-    elif (ckBefore == (-1, 0) and ckAfter == (0, -1)):
-      img = self.snakeTurnLeftImgTab[0]
-    elif (ckBefore == (0, -1) and ckAfter == (1, 0)):
-      img = self.snakeTurnLeftImgTab[1]
-    elif (ckBefore == (1, 0) and ckAfter == (0, 1)):
-      img = self.snakeTurnLeftImgTab[2]
-    elif (ckBefore == (0, 1) and ckAfter == (-1, 0)):
-      img = self.snakeTurnLeftImgTab[3]
-    # Right
-    elif (ckBefore == (0, -1) and ckAfter == (-1, 0)):
-      img = self.snakeTurnRightImgTab[3]
-    elif (ckBefore == (1, 0) and ckAfter == (0, -1)):
-      img = self.snakeTurnRightImgTab[2]
-    elif (ckBefore == (0, 1) and ckAfter == (1, 0)):
-      img = self.snakeTurnRightImgTab[1]
-    elif (ckBefore == (-1, 0) and ckAfter == (0, 1)):
-      img = self.snakeTurnRightImgTab[0]
-    else:
-      img = self.snakeTailImg
-
-    snakeBodyRect = img.get_rect(center = (position.x * TILE_WIDTH + 50 + TILE_WIDTH / 2, position.y * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
-    WINDOW.blit(img, snakeBodyRect)
-
-  def imgVariant(self):
-    for index, tile in enumerate(self.tailTab):
-
-      if index == 0:
-        checkBefore = self.headPosition - self.tailTab[index]
-        checkAfter = self.tailTab[index] - self.tailTab[index + 1]
-        self.imgVariantDraw(checkBefore, checkAfter, tile)
-
-      elif index == len(self.tailTab) - 1:
-        checkBefore = self.tailTab[index - 1] - self.tailTab[index]
-        self.drawTail(checkBefore, tile)
-      
-      else:
-        checkBefore = self.tailTab[index - 1] - self.tailTab[index]
-        checkAfter = self.tailTab[index] - self.tailTab[index + 1]
-        self.imgVariantDraw(checkBefore, checkAfter, tile)
-
-  def move(self):
-    self.tailTab.insert(0,self.headPosition)
-    self.endOfTailLastPosition = self.tailTab.pop()
-
-    if self.newDirection == 'left':
-      self.headPosition = pygame.Vector2(self.headPosition[0] - 1, self.headPosition[1])
-      self.snakeHeadImg = self.snakeHeadImgTab[3]
-
-
-    if self.newDirection == 'right':
-      self.headPosition = pygame.Vector2(self.headPosition[0] + 1, self.headPosition[1])
-      self.snakeHeadImg = self.snakeHeadImgTab[1]
-
-
-    if self.newDirection == 'up':
-      self.headPosition = pygame.Vector2(self.headPosition[0], self.headPosition[1] - 1)
-      self.snakeHeadImg = self.snakeHeadImgTab[0]
-
-
-    if self.newDirection == 'down':
-      self.headPosition = pygame.Vector2(self.headPosition[0], self.headPosition[1] + 1)
-      self.snakeHeadImg = self.snakeHeadImgTab[2]
-
-
-    self.direction = self.newDirection
-
-  def grow(self):
-    self.tailTab.append(self.endOfTailLastPosition)
-
-  def playerInput(self):
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and self.direction != 'down':
-      self.newDirection = 'up'
-
-    elif keys[pygame.K_DOWN] and self.direction != 'up':
-      self.newDirection = 'down'
-
-    elif keys[pygame.K_LEFT] and self.direction != 'right':
-      self.newDirection = 'left'
-
-    elif keys[pygame.K_RIGHT] and self.direction != 'left':
-      self.newDirection = 'right'
-      
-class Apple(pygame.sprite.Sprite):
-  def __init__(self):
-    super().__init__()
-    self.position = pygame.Vector2(1, 1)
-    self.isEatten = True
-    self.appleImg = pygame.image.load('./graphics/apple.png').convert_alpha()
-  
-  def update(self, snakeHead, snakeTail):
-    self.drawApple()
-    if self.isEatten:
-      self.spawn(snakeHead, snakeTail)
-
-  def drawApple(self):
-    appleRect = self.appleImg.get_rect(center = (self.position[0] * TILE_WIDTH + 50 + TILE_WIDTH / 2, self.position[1] * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
-    WINDOW.blit(self.appleImg, appleRect)
-
-  def spawn(self, snakeHead, snakeTail):
-    self.position = snakeHead
-
-    while self.position in snakeTail or self.position == snakeHead:
-      x = randint(0, NUM_OF_COL-1)
-      y = randint(0, NUM_OF_COL-1)
-      self.position = pygame.Vector2(x,y)
-
-    self.isEatten = False
+from player import Snake
 
 
 pygame.init()
@@ -187,8 +17,8 @@ WINDOW_HEIGHT = 700
 PLAY_SURFACE_WIDTH = 600
 PLAY_SURFACE_HEIGHT = 600
 
-NUM_OF_COL = 20
-NUM_OF_ROW = 20
+NUM_OF_COL = 60
+NUM_OF_ROW = 60
 
 TILE_WIDTH = PLAY_SURFACE_WIDTH / NUM_OF_COL
 TILE_HEIGHT = PLAY_SURFACE_HEIGHT / NUM_OF_ROW
@@ -246,21 +76,12 @@ def gameOverCondition(snake):
     return False
   return True
 
-def eatManager(snake, apple):
-  global CURRENT_SCORE
-
-  if snake.headPosition == apple.position:
-    apple.isEatten = True
-    snake.grow()
-    CURRENT_SCORE += 100
-
 def printScore():
   scoreTxt = font.render(f'Score: {CURRENT_SCORE}', True, (0,0,0))
   scoreRect = scoreTxt.get_rect(center = (WINTDOW_WIDTH / 2, 25))
   WINDOW.blit(scoreTxt, scoreRect)
 
-snake = Snake()
-apple = Apple()
+snake = Snake(NUM_OF_COL, NUM_OF_ROW, TILE_WIDTH)
 
 gameActive = False
 
@@ -280,16 +101,13 @@ while True:
     drawBackground()
     printScore()
     drawPlaySurface()
-    eatManager(snake, apple)
 
     gameActive = gameOverCondition(snake)
 
-    snake.update()
-    apple.update(snake.headPosition, snake.tailTab)
+    snake.update(TILE_WIDTH, TILE_HEIGHT, WINDOW)
   else:
     WINDOW.fill((121, 144, 147))
 
-    
     nameTxt = font.render('py-snake', True, (0, 0, 0))
     nameRect = nameTxt.get_rect(center = (WINTDOW_WIDTH/2, WINDOW_HEIGHT/2 - 100))
     WINDOW.blit(nameTxt, nameRect)
@@ -306,9 +124,8 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RETURN]:
       gameActive = True
-      snake = Snake()
-      apple = Apple()
+      snake = Snake(NUM_OF_COL, NUM_OF_ROW, TILE_WIDTH)
       CURRENT_SCORE = 0
 
   pygame.display.update()
-  clock.tick(60)
+  clock.tick(30)
---
+++ Game/player.py
@@ -0,0 +1,147 @@
+import pygame
+
+
+class Snake(pygame.sprite.Sprite):
+  def __init__(self, NUM_OF_COL: int, NUM_OF_ROW: int, TILE_WIDTH: int):
+    super().__init__()
+    self.headPosition = pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2)
+    self.tailTab = [pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2 + 1),pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2 + 2)]
+
+    self.endOfTailLastPosition = pygame.Vector2(NUM_OF_COL//2, NUM_OF_ROW//2)
+    self.direction = 'up'
+    self.newDirection = 'up'
+    self.snakeScale = (TILE_WIDTH / 40)
+
+    self.snakeHeadImg = pygame.image.load('./graphics/head.png').convert_alpha()
+    self.snakeHeadImg = pygame.transform.rotozoom(self.snakeHeadImg, 0, self.snakeScale)
+    self.snakeHeadImgTab = [self.snakeHeadImg, pygame.transform.rotate(self.snakeHeadImg, 270), pygame.transform.rotate(self.snakeHeadImg, 180), pygame.transform.rotate(self.snakeHeadImg, 90)]
+
+    self.snakeBodyImg = pygame.image.load('./graphics/body.png').convert_alpha()
+    self.snakeBodyImg = pygame.transform.rotozoom(self.snakeBodyImg, 0, self.snakeScale)
+    self.snakeBodyImgTab = [self.snakeBodyImg, pygame.transform.rotate(self.snakeBodyImg, 270), pygame.transform.rotate(self.snakeBodyImg, 180), pygame.transform.rotate(self.snakeBodyImg, 90)]
+
+    self.snakeTurnLeftImg = pygame.image.load('./graphics/turn.png').convert_alpha()
+    self.snakeTurnLeftImg = pygame.transform.rotozoom(self.snakeTurnLeftImg, 0, self.snakeScale)
+    self.snakeTurnLeftImgTab = [self.snakeTurnLeftImg, pygame.transform.rotate(self.snakeTurnLeftImg, 270), pygame.transform.rotate(self.snakeTurnLeftImg, 180), pygame.transform.rotate(self.snakeTurnLeftImg, 90)]
+
+    self.snakeTurnRightImgTab = [pygame.transform.flip(self.snakeTurnLeftImgTab[0], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[1], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[2], False, True), pygame.transform.flip(self.snakeTurnLeftImgTab[3], False, True)]
+
+    self.snakeTailImg = pygame.image.load('./graphics/tail.png').convert_alpha()
+    self.snakeTailImg = pygame.transform.rotozoom(self.snakeTailImg, 0, self.snakeScale)
+    self.snakeTailImgTab = [self.snakeTailImg, pygame.transform.rotate(self.snakeTailImg, 270), pygame.transform.rotate(self.snakeTailImg, 180), pygame.transform.rotate(self.snakeTailImg, 90)]
+
+  def update(self, TILE_WIDTH: int, TILE_HEIGHT: int, WINDOW):
+    self.drawSnake(TILE_WIDTH, TILE_HEIGHT, WINDOW)
+    self.playerInput()    
+
+  def drawSnake(self, TILE_WIDTH: int, TILE_HEIGHT: int, WINDOW):
+    snakeHeadRect = self.snakeHeadImg.get_rect(center = (self.headPosition[0] * TILE_WIDTH + 50 + TILE_WIDTH / 2, self.headPosition[1] * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
+    WINDOW.blit(self.snakeHeadImg, snakeHeadRect)
+
+    for tile in self.tailTab:
+      #pygame.draw.rect(WINDOW, (0, 0, 255), pygame.Rect((tile[0] * TILE_WIDTH) + 50, (tile[1] * TILE_HEIGHT) + 50, TILE_WIDTH, TILE_HEIGHT ))
+      self.imgVariant(TILE_WIDTH, TILE_HEIGHT, WINDOW)
+
+  def drawTail(self, ckBefore, position, TILE_WIDTH: int, TILE_HEIGHT: int, WINDOW):
+    if (ckBefore == (1, 0)):
+      img = self.snakeTailImgTab[1]
+    elif (ckBefore == (-1, 0)):
+      img = self.snakeTailImgTab[3]
+    elif (ckBefore == (0, 1)):
+      img = self.snakeTailImgTab[2]
+    elif (ckBefore == (0, -1)):
+      img = self.snakeTailImgTab[0]
+
+    snakeBodyRect = img.get_rect(center = (position.x * TILE_WIDTH + 50 + TILE_WIDTH / 2, position.y * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
+    WINDOW.blit(img, snakeBodyRect)
+
+  def imgVariantDraw(self, ckBefore, ckAfter, position,  TILE_WIDTH: int, TILE_HEIGHT: int, WINDOW):
+    if (ckBefore == ckAfter == (-1, 0)):
+      img = self.snakeBodyImgTab[1]
+    elif (ckBefore == ckAfter == (1, 0)):
+      img = self.snakeBodyImgTab[3]
+    elif (ckBefore == ckAfter == (0, -1)):
+      img = self.snakeBodyImgTab[2]
+    elif (ckBefore == ckAfter == (0, 1)):
+      img = self.snakeBodyImgTab[0]
+    elif (ckBefore == (-1, 0) and ckAfter == (0, -1)):
+      img = self.snakeTurnLeftImgTab[0]
+    elif (ckBefore == (0, -1) and ckAfter == (1, 0)):
+      img = self.snakeTurnLeftImgTab[1]
+    elif (ckBefore == (1, 0) and ckAfter == (0, 1)):
+      img = self.snakeTurnLeftImgTab[2]
+    elif (ckBefore == (0, 1) and ckAfter == (-1, 0)):
+      img = self.snakeTurnLeftImgTab[3]
+    # Right
+    elif (ckBefore == (0, -1) and ckAfter == (-1, 0)):
+      img = self.snakeTurnRightImgTab[3]
+    elif (ckBefore == (1, 0) and ckAfter == (0, -1)):
+      img = self.snakeTurnRightImgTab[2]
+    elif (ckBefore == (0, 1) and ckAfter == (1, 0)):
+      img = self.snakeTurnRightImgTab[1]
+    elif (ckBefore == (-1, 0) and ckAfter == (0, 1)):
+      img = self.snakeTurnRightImgTab[0]
+    else:
+      img = self.snakeTailImg
+
+    snakeBodyRect = img.get_rect(center = (position.x * TILE_WIDTH + 50 + TILE_WIDTH / 2, position.y * TILE_HEIGHT + 50  + TILE_HEIGHT / 2))
+    WINDOW.blit(img, snakeBodyRect)
+
+  def imgVariant(self, TILE_WIDTH: int, TILE_HEIGHT: int, WINDOW):
+    for index, tile in enumerate(self.tailTab):
+
+      if index == 0:
+        checkBefore = self.headPosition - self.tailTab[index]
+        checkAfter = self.tailTab[index] - self.tailTab[index + 1]
+        self.imgVariantDraw(checkBefore, checkAfter, tile, TILE_WIDTH, TILE_HEIGHT, WINDOW)
+
+      elif index == len(self.tailTab) - 1:
+        checkBefore = self.tailTab[index - 1] - self.tailTab[index]
+        self.drawTail(checkBefore, tile, TILE_WIDTH, TILE_HEIGHT, WINDOW)
+      
+      else:
+        checkBefore = self.tailTab[index - 1] - self.tailTab[index]
+        checkAfter = self.tailTab[index] - self.tailTab[index + 1]
+        self.imgVariantDraw(checkBefore, checkAfter, tile, TILE_WIDTH, TILE_HEIGHT, WINDOW)
+
+  def move(self):
+    self.tailTab.insert(0,self.headPosition)
+
+    if self.newDirection == 'left':
+      self.headPosition = pygame.Vector2(self.headPosition[0] - 1, self.headPosition[1])
+      self.snakeHeadImg = self.snakeHeadImgTab[3]
+
+
+    if self.newDirection == 'right':
+      self.headPosition = pygame.Vector2(self.headPosition[0] + 1, self.headPosition[1])
+      self.snakeHeadImg = self.snakeHeadImgTab[1]
+
+
+    if self.newDirection == 'up':
+      self.headPosition = pygame.Vector2(self.headPosition[0], self.headPosition[1] - 1)
+      self.snakeHeadImg = self.snakeHeadImgTab[0]
+
+
+    if self.newDirection == 'down':
+      self.headPosition = pygame.Vector2(self.headPosition[0], self.headPosition[1] + 1)
+      self.snakeHeadImg = self.snakeHeadImgTab[2]
+
+
+    self.direction = self.newDirection
+
+  def grow(self):
+    self.tailTab.append(self.endOfTailLastPosition)
+
+  def playerInput(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and self.direction != 'down':
+      self.newDirection = 'up'
+
+    elif keys[pygame.K_DOWN] and self.direction != 'up':
+      self.newDirection = 'down'
+
+    elif keys[pygame.K_LEFT] and self.direction != 'right':
+      self.newDirection = 'left'
+
+    elif keys[pygame.K_RIGHT] and self.direction != 'left':
+      self.newDirection = 'right'
