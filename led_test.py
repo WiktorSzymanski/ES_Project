57f4f2fb78dabff8f6fdf497422f9246ee118942
DarkVikos <darkvikos@gmail.com>
2023-01-03 15:01:00 +0100

    led test

--- led_test.py
+++ led_test.py
@@ -8,8 +8,8 @@ GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
 try:
   while True:
     GPIO.output(26, GPIO.HIGH)
-    time.sleep(1)
+    time.sleep(10)
     GPIO.output(26, GPIO.LOW)
-    time.sleep(1)
+    time.sleep(10)
 except KeyboardInterrupt:
   GPIO.clenup()
