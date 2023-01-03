62b63be731eee96649105e9e4deddd90708cff43
DarkVikos <darkvikos@gmail.com>
2023-01-03 14:59:45 +0100

    led test

--- led_test.py
+++ led_test.py
@@ -1,11 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
 
-while True:
-  GPIO.output(26, GPIO.HIGH)
-  time.sleep(1)
-  GPIO.output(26, GPIO.LOW)
-  time.sleep(1)
+try:
+  while True:
+    GPIO.output(26, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(26, GPIO.LOW)
+    time.sleep(1)
+except KeyboardInterrupt:
+  GPIO.clenup()
