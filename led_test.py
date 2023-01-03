abedf7c746a093abad775f845e329efc3c06e799
DarkVikos <darkvikos@gmail.com>
2023-01-03 14:51:28 +0100

    led test

--- led_test.py
+++ led_test.py
@@ -1,10 +1,11 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
 
 while True:
   GPIO.output(26, GPIO.HIGH)
-  sleep(1)
+  time.sleep(1)
   GPIO.output(26, GPIO.LOW)
-  sleep(1)
+  time.sleep(1)
